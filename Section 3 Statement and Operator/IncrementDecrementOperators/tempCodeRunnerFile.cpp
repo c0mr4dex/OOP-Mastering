19fec7dba874aa69f7099412848630fac6a99d4d
c0mr4de <c0mr4dex@hotmail.com>
2023-05-19 20:47:25 +0700

    update

--- Section 3 Statement and Operator/IncrementDecrementOperators/tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-   counter = 10;
-   result = 0;
-
-   cout << "Counter : " << counter << endl;
-
-   result = counter++ + 10;  // Note the post increment
-       
-   cout << "Counter : " << counter << endl;
-   cout << "Result : " << result << endl;
